c56064657f36aa7389fbc350b927574241583f1c
choi.huang <choi.huang@valorosoltd.com>
2021-02-16 22:28:04 +0800

    commit

---
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/CoronaVirusTrackerApplication.java
@@ -0,0 +1,16 @@
+package io.javabrains.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication(
+)
+public class CoronaVirusTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaVirusTrackerApplication.class, args);
+    }
+
+}
